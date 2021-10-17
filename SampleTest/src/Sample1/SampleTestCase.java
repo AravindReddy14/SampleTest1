5b81164866d7dcc68666b5bbce3a2ab59a819be8
Srinivas <Srinivas@DESKTOP-R6H5VFO>
2021-10-17 15:08:18 +0530

    Addition Program of two numbers

---
+++ SampleTest/src/Sample1/SampleTestCase.java
@@ -0,0 +1,10 @@
+package Sample1;
+
+public class SampleTestCase {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int num = a+b;
+		System.out.println("Addition of two numbers is :"+num);
+	}
+}
